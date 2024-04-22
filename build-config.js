cf69ea787d711228594b6aab8d67d471891b2372
Arjun <arjunsuresh1987@gmail.com>
2024-04-22 22:17:05 +0530

    CK Editor on 20240422

---
+++ build-config.js
@@ -0,0 +1,94 @@
+﻿/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license/
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/9bd159f1e93b7de1b7b4c67c710f2932
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/9bd159f1e93b7de1b7b4c67c710f2932
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'standard',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.nvmrc',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender-runner.config.json',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package-lock.json',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'autosave' : 1,
+		'basicstyles' : 1,
+		'blockquote' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'contextmenu' : 1,
+		'emoji' : 1,
+		'eqneditor' : 1,
+		'filebrowser' : 1,
+		'find' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'image' : 1,
+		'image2' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'link' : 1,
+		'list' : 1,
+		'liststyle' : 1,
+		'magicline' : 1,
+		'maximize' : 1,
+		'pbckcode' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'sourcearea' : 1,
+		'sourcedialog' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tabletools' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
---
+++ config.js
@@ -0,0 +1,66 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		 { name: 'pbckcode' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'blocks', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'htmlSource' },
+		{ name: 'about' }
+	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript,Anchor';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	//config.extraPlugins ='iframe,notification';
+	config.removePlugins ='sourcedialog';
+	config.extraPlugins ='emoji';
+	config.scayt_autoStartup = true;
+	config.extraAllowedContent = 'pre[*]{*}(*),iframe[*]'; // add other rules here
+	//      config.allowedContent = 'pre[*]{*}(*)'; // add other rules here
+
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+	config.pbckcode = {
+		// An optional class to your pre tag.
+		cls : '',
+
+		// The syntax highlighter you will use in the output view
+		highlighter : 'PRETTIFY',
+
+		// An array of the available modes for you plugin.
+		// The key corresponds to the string shown in the select tag.
+		// The value correspond to the loaded file for ACE Editor.
+		modes :  [ ['C/C++', 'c_cpp'], ['Java', 'java'], ['SQL', 'sql'], ['HTML', 'html'], ['Python', 'python'], ['SH', 'sh'], ['Text', 'text']],
+
+		// The theme of the ACE Editor of the plugin.
+		theme : 'textmate',
+
+		// Tab indentation (in spaces)
+		tab_size : '4'
+	};
+};
